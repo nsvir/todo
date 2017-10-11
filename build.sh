fc56080e4e3fe12601594281f3560bda2edd3d56
Svirchevsky Nicolas <n.svirchevsky@gmail.com>
2017-10-11 13:26:54 +0200

    Fixed: acceptance test requierements

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ case $1 in
 	do find -name "$junk" -exec rm -rfv {} 2>/dev/null \;
 	done;;
     acceptance)
+	[ ! $(which geckodriver) ] \
+	    && echo "geckodriver must be installed and in \$PATH\nhttps://github.com/mozilla/geckodriver/releases" \
+	    && exit
 	python -m robot acceptance;;
     test)
 	nosetests --with-coverage \
