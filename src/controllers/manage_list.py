9019cd37b321a17e206e62a5175b60486ecc29d5
HeleneMyr <mey.helene@gmail.com>
2017-10-12 23:22:38 +0200

    ajout list dans base de données

--- src/controllers/manage_list.py
+++
@@ -1,9 +0,0 @@
-from bottle import Bottle, request, template, debug, static_file
-import os, sys
-loginApp = Bottle()
-
-debug(True)
-
-@loginApp.route('/addList/:listname')
-def addList(listname):
-
