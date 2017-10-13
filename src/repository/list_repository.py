a2f99513d29465e5cd3e6871f8723684b386cddb
HeleneMyr <mey.helene@gmail.com>
2017-10-13 22:50:18 +0200

    fix bug in view + test before

--- src/repository/list_repository.py
+++ src/repository/list_repository.py
@@ -8,18 +8,14 @@ class ListRepository:
         self.connection = connection
 
     def add_list_into_repository(self, name):
-        with self.connection:
-            cur = self.connection.cursor()
-            cur.execute("INSERT into lists values ('" + name +"')")
-            rows = cur.fetchall()
-            return rows
+        cur = self.connection.cursor()
+        cur.execute("INSERT into lists values ('" + name +"')")
 
     def get_list_repository(self):
-        with self.connection:
-            cur = self.connection.cursor()
-            cur.execute("SELECT * from lists")
-            rows = cur.fetchall()
-            lst = []
-            for row in rows:
-                lst.append(row[0])
-            return lst
+        cur = self.connection.cursor()
+        cur.execute("SELECT * from lists")
+        rows = cur.fetchall()
+        lst = []
+        for row in rows:
+            lst.append(row[0])
+        return lst
