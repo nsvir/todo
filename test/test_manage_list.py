4c0a89e5445808610bafd711829b66a267d1ed12
HeleneMyr <mey.helene@gmail.com>
2017-10-13 23:56:36 +0200

    ajout test controller manage_list

--- test/test_manage_list.py
+++ test/test_manage_list.py
@@ -1,13 +1,23 @@
 from controller.manage_list import *
 import unittest
-import mockito
-
+from mockito import when, mock
+from bottle import template
 
 class TestAddListTodoController(unittest.TestCase):
 
     def test_add_list_controller(self):
-        listTodoService = mockito.mock()
+        listTodoService = mock()
         manageList = ManageList(listTodoService)
-        mockito.when(listTodoService).add_list('name').thenRaise(ValueError())
+        when(listTodoService).add_list('name').thenRaise(ValueError())
         
-        #self.assertRaises(ValueError, manageList.addList, 'name')
+        self.assertRaises(ValueError, manageList.addList, 'name')
+
+class TestListSettingsTodoController(unittest.TestCase):
+
+    def test_settings_list_controller(self):
+        listTodoService = mock()
+        manageList = ManageList(listTodoService)
+        when(listTodoService).get_list().thenReturn(['name'])
+        output = manageList.listSettings('name')
+        
+        self.assertEquals(template('src/web/template/settingsList.tpl', lists=['name'], list='name'), output)
